3a90820b381b64564aeb4e95fbd454a767247193
milmilkim <soundkim27@gmail.com>
2023-07-13 18:00:50 +0900

    feat: router

---
+++ src/@types/route.ts
@@ -0,0 +1,7 @@
+export enum route {
+  Chapter1 = '/ch1',
+  Chapter2 = '/ch2',
+  Chapter3 = '/ch3',
+  Chapter4 = '/ch4',
+  Chapter5 = '/ch5',
+}
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,36 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Index from '@/pages/Index';
+import Chapter1 from '@/contents/chapter1';
+import { route } from '@/@types/route';
+import Chapter2 from '@/contents/chapter2';
+import Chapter3 from '@/contents/chapter3';
+import Chapter4 from '@/contents/chapter4';
+import Chapter5 from '@/contents/Chapter5';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Index />
+    element: <Index />,
+  },
+  {
+    path: route.Chapter1,
+    element: <Chapter1 />,
+  },
+  {
+    path: route.Chapter2,
+    element: <Chapter2 />,
+  },
+  {
+    path: route.Chapter3,
+    element: <Chapter3 />,
+  },
+  {
+    path: route.Chapter4,
+    element: <Chapter4 />,
+  },
+  {
+    path: route.Chapter5,
+    element: <Chapter5 />,
   },
 ]);
 
