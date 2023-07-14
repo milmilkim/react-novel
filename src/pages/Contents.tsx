07d5cb166bc84690ebb0e95f8fe9568eebb11cbb
milmilkim <soundkim27@gmail.com>
2023-07-14 17:17:17 +0900

    불현듯 깨달음을 얻다

--- src/pages/Contents.tsx
+++ src/pages/Contents.tsx
@@ -16,18 +16,18 @@ const Contents = () => {
     try {
       if (!id) throw new Error('id가 존재하지 않습니다');
 
+      const base = import.meta.env.DEV ? '/' : `/${APP.GITHUB_REPO_NAME}`;
+
       const idNumber = id.replace(PAGE.PREFIX, '');
 
-      const res = await fetch(`/files/${idNumber}.html`);
+      const res = await fetch(`${base}/files/${idNumber}.html`);
 
       if (!res.ok) {
         throw new Error('파일을 불러오지 못했습니다');
       }
 
-      const base = import.meta.env.DEV ? '' : `${APP.GITHUB_REPO_NAME}`;
-      console.log('base:', import.meta.env.DEV);
       const text = await res.text();
-      const html = text.replace(/\/images\//g, `/${base}/files/images/`);
+      const html = text.replace(/\/images\//g, `${base}/files/images/`);
 
       setHtml(html);
     } catch (err) {
